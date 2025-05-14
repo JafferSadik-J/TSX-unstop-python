5c846ef794f67c7dbf730bf0e0d9026febf473a9
JafferSadik-J <jaffer1166bunny@gmail.com>
2025-05-14 07:34:39 +0530

    Create greetings.py

---
+++ greetings.py
@@ -0,0 +1,5 @@
+# Prompt the user to enter their name using the input() function
+name = input("Enter your name: ")  # Takes user input and stores it in the variable 'name'
+
+# Print a personalized greeting message using the name provided by the user
+print("Hello, " + name)  # Displays the greeting
