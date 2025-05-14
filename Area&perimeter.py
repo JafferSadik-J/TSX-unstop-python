df1c2bd1c58f68f5a06822bd5fbf5c361512d267
JafferSadik-J <jaffer1166bunny@gmail.com>
2025-05-14 07:45:18 +0530

    Update Area&perimeter.py

--- Area&perimeter.py
+++ Area&perimeter.py
@@ -1,8 +1,8 @@
 # This script calculates the area and perimeter of a rectangle
 
 # Define the length and width of the rectangle
-length = input(int("Enter the Length of rectangle")  # Takes user input for length
-width = input(int("Enter the Width of rectangle")   # Takes user input for width
+length = int(input("Enter the Length of rectangle")  # Takes user input for length
+width = int(input("Enter the Width of rectangle")   # Takes user input for width
 
 # Calculate the area
 area = length * width
