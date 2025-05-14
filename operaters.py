6c68300178a27aa81243e12782d7eb1626830ac5
JafferSadik-J <jaffer1166bunny@gmail.com>
2025-05-14 07:44:29 +0530

    Create operaters.py

---
+++ operaters.py
@@ -0,0 +1,17 @@
+# Arithmetic Operators
+a = int(input("Enter the first number"))
+b = int(input("Enter the second number"))
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+print("Exponentiation:", a ** b)
+print("Floor Division:", a // b)
+
+# Logical Operators
+x = True
+y = False
+print("\nLogical AND:", x and y)
+print("Logical OR:", x or y)
+print("Logical NOT (x):", not x)
